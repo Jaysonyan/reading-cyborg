a5a9b48f1157a882773c24cc7950f23f4683e0b1
Jayson Yan <jj2yan@uwaterloo.ca>
2018-02-24 06:02:26 -0500

    Fully scraped site with story txt

--- webScraper.py
+++ webScraper.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup as soup
 from urllib2 import urlopen
 import re
+import os
 
+os.chdir('stories')
 webURL = "https://americanliterature.com/short-stories-for-children"
 homepage = urlopen(webURL)
 homepageHTML = homepage.read()
@@ -12,62 +14,79 @@ rawLinks = homeSoup.findAll("figure", {"class":"imgcs"})
 links = []
 paragraph = []
 
-# for item in rawLinks:
-#     storyURL = 'https://americanliterature.com'+ item.a['href']
-#     storypage = urlopen(storyURL)
-#     storyHTML = storypage.read()
-#     storypage.close()
-#     storySoup = soup(storyHTML, "html.parser")
-#     storyInfo = storySoup.findAll("div", {"class":"jumbotron"})
-#     title = storyInfo[0].h1.cite.get_text()
-#     storyContent = storySoup.find_all(["p", "pre"])
-#     for i in range(0,len(storyContent)-1):
-#         storyContent[i] = storyContent[i].get_text()
-#     storyContent = [x for x in storyContent if x != '']
-#     del storyContent[-1]
+for item in rawLinks:
+    storyURL = 'https://americanliterature.com'+ item.a['href']
+    storypage = urlopen(storyURL)
+    storyHTML = storypage.read()
+    storypage.close()
+
+    storySoup = soup(storyHTML, "html.parser")
+
+    storyInfo = storySoup.findAll("div", {"class":"jumbotron"})
+    try:
+        title = storyInfo[0].h1.cite.get_text()
+        storyContent = storySoup.find_all(["p", "pre"])
+        garbage = []
+        for i in range(0,len(storyContent)):
+            storyContent[i] = storyContent[i].encode('utf-8')
+            if "/a" in storyContent[i]:
+                garbage.append(i)
+            storyContent[i] = re.sub('<[^>]+>', '', storyContent[i])
+
+        # for i in range(len(garbage)-1,0, -1):
+        #     del storyContent[i]
+
+        storyContent = [x for x in storyContent if x != '']
+
+
+        filename = "%s.txt" %title
+        # for paragraph in storyContent:
+        #     print paragraph+'\n'
+
+        storyText = open(filename , 'w')
+
+        storyText.write(title+",\n")
+
+        for paragraph in storyContent:
+            storyText.write(paragraph)
+            storyText.write('\n')
+        storyText.close()
+    except:
+        pass
+
+
+# storyURL = 'https://americanliterature.com'+ rawLinks[1].a['href']
+# storypage = urlopen(storyURL)
+# storyHTML = storypage.read()
+# storypage.close()
 #
-#     filename = "%s.txt" % title
-#     storyText = open(filename , 'w')
+# storySoup = soup(storyHTML, "html.parser")
 #
-#     storyText.write(title+",\n")
-#     for paragraph in storyContent:
-#         storyText.write(paragraph)
-#         storyText.write('\n')
-#     storyText.close()
-
-
-storyURL = 'https://americanliterature.com'+ rawLinks[1].a['href']
-storypage = urlopen(storyURL)
-storyHTML = storypage.read()
-storypage.close()
-
-storySoup = soup(storyHTML, "html.parser")
-
-storyInfo = storySoup.findAll("div", {"class":"jumbotron"})
-title = storyInfo[0].h1.cite.get_text()
-storyContent = storySoup.find_all(["p", "pre"])
-garbage = []
-for i in range(0,len(storyContent)):
-    storyContent[i] = storyContent[i].encode('utf-8')
-    if "/a" in storyContent[i]:
-        garbage.append(i)
-    storyContent[i] = re.sub('<[^>]+>', '', storyContent[i])
-
-# for i in range(len(garbage)-1,0, -1):
-#     del storyContent[i]
-
-storyContent = [x for x in storyContent if x != '']
-
-
-filename = "%s.txt" %title
+# storyInfo = storySoup.findAll("div", {"class":"jumbotron"})
+# title = storyInfo[0].h1.cite.get_text()
+# storyContent = storySoup.find_all(["p", "pre"])
+# garbage = []
+# for i in range(0,len(storyContent)):
+#     storyContent[i] = storyContent[i].encode('utf-8')
+#     if "/a" in storyContent[i]:
+#         garbage.append(i)
+#     storyContent[i] = re.sub('<[^>]+>', '', storyContent[i])
+#
+# # for i in range(len(garbage)-1,0, -1):
+# #     del storyContent[i]
+#
+# storyContent = [x for x in storyContent if x != '']
+#
+#
+# filename = "%s.txt" %title
+# # for paragraph in storyContent:
+# #     print paragraph+'\n'
+#
+# storyText = open(filename , 'w')
+#
+# storyText.write(title+",\n")
+#
 # for paragraph in storyContent:
-#     print paragraph+'\n'
-
-storyText = open(filename , 'w')
-
-storyText.write(title+",\n")
-
-for paragraph in storyContent:
-    storyText.write(paragraph)
-    storyText.write('\n')
-storyText.close()
+#     storyText.write(paragraph)
+#     storyText.write('\n')
+# storyText.close()
